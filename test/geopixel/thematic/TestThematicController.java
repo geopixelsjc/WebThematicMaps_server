beefa5763f1a22057bcd0785f0848697cd2318a3
Paulo Luan <pauloluan.inova@gmail.com>
2015-08-24 10:46:53 -0300

    added tests for generate colors by range

--- test/geopixel/thematic/TestThematicController.java
+++ test/geopixel/thematic/TestThematicController.java
@@ -5,7 +5,10 @@ package geopixel.thematic;
 
 import static org.junit.Assert.assertNotNull;
 
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -29,8 +32,28 @@ public class TestThematicController {
                 
                 double[] values = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
                 int breaks = 2;
-                
                 List<Double> result = controller.calculateRanges(values, breaks);
-                assertNotNull(result);                
+                assertNotNull(result);
+        }
+        
+        @Test
+        public void testGenerateColorsByRange() {
+                double[] values = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+                int breaks = 4;
+                List<Double> calculatedRanges = controller.calculateRanges(values, breaks);
+                assertNotNull(calculatedRanges);
+                
+                HashMap<String, List<Double>> colors = controller.generateColorsByRange(calculatedRanges);
+                
+                Iterator it = colors.entrySet().iterator();
+                
+                while (it.hasNext()) {
+                        Map.Entry pair = (Map.Entry) it.next();
+                        String color = (String) pair.getKey();
+                        List<Double> ranges = (List<Double>) pair.getValue();
+                }
+                
+                assertNotNull(colors);
+                
         }
 }
