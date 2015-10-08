35947642c4d1e3361b15708c55f9d1fe8b550527
David <dvd.pansardis@gmail.com>
2015-10-08 11:26:29 -0300

    Add scripts import database

---
+++ Scripts DataBase/scripts_load_datas.sql
@@ -0,0 +1,9 @@
+﻿--Verificador de nomes e indentificadores
+select distinct nome, identificador from tab_valores_teste group by nome, identificador 
+select distinct identificador from tab_valores_teste group by identificador 
+
+--Limpeza da Tabela
+delete from tab_valores_teste
+
+--Insert da tabela indicadores
+insert into tab_indicadores_teste (nome, indicador) select distinct nome, identificador from tab_valores_teste group by nome, identificador
